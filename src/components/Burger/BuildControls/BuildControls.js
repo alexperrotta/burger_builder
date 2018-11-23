6c0502c7afe110b391a02e98fcb06d4c3dee9ade
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-11-23 18:55:49 -0500

    added modal

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -22,7 +22,7 @@ const buildControls = (props) => (
         ))}
         <button 
         className={classes.OrderButton}
-        disabled={!props.purchaseable}>ORDER NOW</button>
+        disabled={!props.purchaseable} onClick={props.ordered}>ORDER NOW</button>
     </div>
 );
 
